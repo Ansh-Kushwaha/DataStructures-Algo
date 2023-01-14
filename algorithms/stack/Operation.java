b9bde12a190791347c1cddc574bd88155ef9a032
unknown <ansh.kushwaha26@gmail.com>
2023-01-14 17:11:11 +0530

    Added stack reverse algo (#1)

---
+++ algorithms/stack/Operation.java
@@ -0,0 +1,46 @@
+package algorithms.stack;
+
+import stack.linkedstack.LStack;
+import stack.staticstack.SStack;
+
+public class Operation<T> {
+	public SStack<T> tempS;
+	public LStack<T> tempL;
+	public Operation() { }
+	
+	public int size(SStack<T> stack) {
+		tempL = new LStack<T>();
+		int s = 0;
+		while(!stack.isEmpty()) {
+			s++;
+			tempL.push(stack.peek());
+			stack.pop();
+		}
+		
+		while(!tempL.isEmpty()) {
+			stack.push(tempL.peek());
+			tempL.pop();
+		}
+		return s;
+	}
+	
+	public int size(LStack<T> stack) {
+		tempL = new LStack<T>();
+		int s = 0;
+		while(!stack.isEmpty()) {
+			s++;
+			tempL.push(stack.peek());
+			stack.pop();
+		}
+		
+		while(!tempL.isEmpty()) {
+			stack.push(tempL.peek());
+			tempL.pop();
+		}
+		return s;
+	}
+	
+	public void reverse(SStack<T> stack) {
+		tempS = new SStack<T>();
+	}
+}
